5d9bdbfd382a62cf006148c15b3d45e17050f634
Иван <iv.nikolaev@tensor.ru>
2021-10-26 15:09:48 +0300

    Revert "Николаев Тест"
    Откат тестового коммита

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,49 +3,18 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {
-    let sum = 0;
-    for (let i = 0; i <= end; i++) {
-        if (i % 2 == 0) {
-            sum += i;
-        }
-    }
-    return sum;
-}
+export function rangeSum(start, end) {}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {
-    let count = 0;
-    let b = a;
-    do {
-        debugger;
-        b = b / 2;
-        count += 1;
-        console.log(b);
-    } while (b > 0.1);
-    return count;
-}
+export function iterationCount(a) {}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {
-    let str = '';
-    let count = 1;
-    for (let i = 0; i < message.length; i++) {
-        if (count != 3) {
-            str += message[i];
-            count += 1;
-        } else {
-            str += '_';
-            count = 1;
-        }
-    }
-    return str;
-}
+export function symbolsReplace(message) {}
